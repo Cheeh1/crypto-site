2e7dd7b7ae84366083d9e82211a656d7c2596428
Divine Edwin <edwindivine5@gmail.com>
2022-11-07 13:05:38 +0100

    First Commit
    created components and did some stylings..project 60% complete

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Product from './components/Product'
+import Crypto from './components/Crypto'
+
+function App() {
+
+  return (
+    <div>
+      < Product />
+      < Crypto />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Crypto.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+
+function Crypto() {
+ return (
+    <div>
+        <h2>Trade securely and market the high growth cryptocurrencies.</h2>
+       <div>
+            <div>
+                <img src="" alt="" />
+                <h3></h3>
+                <p></p>
+                <div>
+                    <p></p>
+                    <img src="" alt="" />
+                </div>
+            </div>
+
+            <div>
+                <img src="" alt="" />
+                <h3></h3>
+                <p></p>
+                <div>
+                    <p></p>
+                    <img src="" alt="" />
+                </div>
+            </div>
+            
+            <div>
+                <img src="" alt="" />
+                <h3></h3>
+                <p></p>
+                <div>
+                    <p></p>
+                    <img src="" alt="" />
+                </div>
+            </div>
+       </div>
+    </div>
+ )
+}
+export default Crypto;
